e35325b154c5c919574b6630f1e5856727711e11
Joey <t1616joy@yahoo.com.tw>
2022-03-03 19:49:14 +0800

    tet

--- CWB_process_data.py
+++ CWB_process_data.py
@@ -19,8 +19,6 @@ sac_path = f"/home/joey/緬甸BH_ubuntu/dataset/{year}/{mon}/"
 asc_path = f"/home/joey/緬甸BH_ubuntu/{year}_output/{mon}/"
 
 
-
-
 os.chdir(f"{sac_path}")
 file_name = glob.glob("*H1.sac")
 file_name.sort()
